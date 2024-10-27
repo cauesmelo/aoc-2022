c61fd66b8a4b7c628df946f0542b9fa76c5189ca
cauesmelo <cauesmelo@gmail.com>
2024-10-27 19:54:36 -0300

    add d1p1

---
+++ aoc2022/days/d1/p1.py
@@ -0,0 +1,14 @@
+from aoc2022.utils.get_lines import get_lines
+
+
+lines = get_lines("i2.txt")
+
+elfs = [0]
+
+for line in lines:
+    if line != "":
+        elfs[-1] += int(line)
+    else:
+        elfs.append(0)
+
+print(max(elfs))
